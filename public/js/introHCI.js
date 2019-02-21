449007148b23cdc9da932b4038f55c49a8b58acc
Stig Terrebonne <stigterrebonne@gmail.com>
2019-02-21 08:08:03 -0800

    update wqith analytics

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,15 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$('.likeCtr').click(clickLike);
+}
+
+function clickLike(e){
+	e.preventDefault();
+	console.log("made it");
+	gtag('create', 'UA-134624562-1', 'auto');
+	gtag('send', 'event', 'like', 'click');
+	gtag('event', 'click', {
+		event_category: 'like'
+	});
 }
